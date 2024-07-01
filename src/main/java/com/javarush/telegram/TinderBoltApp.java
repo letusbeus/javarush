84524cee530fa2026b611a1a7f421d8a1e9cdda0
letusbeus <letusbeus@mail.ru>
2024-07-01 23:34:24 +0300

    Added chatGPT mode

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -16,6 +16,9 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
     public static final String TELEGRAM_BOT_TOKEN = Data.TG_BOT_TOKEN; //TODO: добавь токен бота в кавычках
     public static final String OPEN_AI_TOKEN = Data.OPEN_AI_TOKEN; //TODO: добавь токен ChatGPT в кавычках
 
+    private ChatGPTService chatGPT = new ChatGPTService(OPEN_AI_TOKEN);
+    private DialogMode currentMode = null;
+
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
     }
@@ -26,9 +29,27 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
         String message = getMessageText();
 
         if (message.equals("/start")) {
+            currentMode = DialogMode.MAIN;
             sendPhotoMessage("main");
             String text = loadMessage("main");
             sendTextMessage(text);
+
+            showMainMenu(Data.MAIN_MENU_COMMANDS); // вынесены в отдельный файл
+            return;
+        }
+
+        if (message.equals("/gpt")) {
+            currentMode = DialogMode.GPT;
+            sendPhotoMessage("gpt");
+            String text = loadMessage("gpt");
+            sendTextMessage(text);
+            return;
+        }
+
+        if (currentMode == DialogMode.GPT) {
+            String prompt = loadPrompt("gpt");
+            String answer = chatGPT.sendMessage(prompt, message);
+            sendTextMessage(answer);
             return;
         }
 
